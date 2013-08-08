7216d98f0d6d458c0d5bc9a0c409e00014b2e7a0
Dan Dofter <dan@dofter.com>
2013-08-08 01:12:03 -0400

    Added Sublime Text 3 config

--- install.sh
+++ install.sh
@@ -7,11 +7,11 @@ ln -s $dotfiles_dir/irbrc $HOME/.irbrc
 mkdir -p $HOME/.ssh
 ln -s $dotfiles_dir/ssh/config $HOME/.ssh/config
 
-for file in $dotfiles_dir/sublime_text_2/*
+for file in $dotfiles_dir/sublime_text_3/*
 do
   if [ -f "$file" ]
   then
-    ln -s $file $HOME/Library/Application\ Support/Sublime\ Text\ 2/Packages/User
+    ln -s $file $HOME/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
   fi
 done
 
