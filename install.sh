0759b140a744e31b36599b61ac2c07fae4cf7788
Dan Dofter <dan@dofter.com>
2013-11-09 19:07:10 -0500

    Create SublimeText config dir

--- install.sh
+++ install.sh
@@ -10,11 +10,12 @@ ln -s $dotfiles_dir/gitignore_global $HOME/.gitignore_global
 mkdir -p $HOME/.ssh
 ln -s $dotfiles_dir/ssh/config $HOME/.ssh/config
 
+mkdir -p "$HOME/Library/Application\ Support/Sublime\ Text\ 3/Packages/User"
 for file in $dotfiles_dir/sublime_text_3/*
 do
   if [ -f "$file" ]
   then
-    ln -s $file $HOME/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
+    ln -s $file "$HOME/Library/Application\ Support/Sublime\ Text\ 3/Packages/User"
   fi
 done
 
