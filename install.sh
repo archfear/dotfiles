2de266a38be92112c2f6e71bda9208f10077e270
Dan Dofter <dan@dofter.com>
2014-02-16 06:05:32 -0500

    Added psqlrc

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ ln -s $dotfiles_dir/irbrc $HOME/.irbrc
 ln -s $dotfiles_dir/gemrc $HOME/.gemrc
 ln -s $dotfiles_dir/gitconfig $HOME/.gitconfig
 ln -s $dotfiles_dir/gitignore_global $HOME/.gitignore_global
+ln -s $dotfiles_dir/psqlrc $HOME/.psqlrc
 
 mkdir -p $HOME/.ssh
 ln -s $dotfiles_dir/ssh/config $HOME/.ssh/config
