3984bda65c9876ede5b0ac592a311e91bdfcd947
Dan Dofter <dan@dofter.com>
2014-03-16 19:22:29 -0400

    Added rvmrc

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ dotfiles_dir="$HOME/.dotfiles"
 
 ln -s $dotfiles_dir/irbrc $HOME/.irbrc
 ln -s $dotfiles_dir/gemrc $HOME/.gemrc
+ln -s $dotfiles_dir/rvmrc $HOME/.rvmrc
 ln -s $dotfiles_dir/gitconfig $HOME/.gitconfig
 ln -s $dotfiles_dir/gitignore_global $HOME/.gitignore_global
 ln -s $dotfiles_dir/psqlrc $HOME/.psqlrc
