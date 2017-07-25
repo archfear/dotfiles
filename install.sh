4441ea4841eb80aa829a364689ad455076385d6e
Dan Dofter <dan@dofter.com>
2017-07-24 22:19:34 -0700

    Removed sublime config

--- install.sh
+++ install.sh
@@ -12,13 +12,4 @@ ln -s $dotfiles_dir/psqlrc $HOME/.psqlrc
 mkdir -p $HOME/.ssh
 ln -s $dotfiles_dir/ssh/config $HOME/.ssh/config
 
-mkdir -p "$HOME/Library/Application Support/Sublime Text 3/Packages/User"
-for file in $dotfiles_dir/sublime_text_3/*
-do
-  if [ -f "$file" ]
-  then
-    ln -s $file "$HOME/Library/Application Support/Sublime Text 3/Packages/User"
-  fi
-done
-
 exit 0
